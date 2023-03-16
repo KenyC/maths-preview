0b1e0ea56792f16f908ac249e2335a2c524025c2
Keny C <keny.chatain@mailbox.org>
2023-03-16 12:57:43 +0100

    ST plugin: user's selection is used as the initial formula in GUI (allow modification of existing formulas) + removed 'shell=True' which lead Python to ignore command line argument

--- clients/sublime-text/insert_formula.py
+++ clients/sublime-text/insert_formula.py
@@ -7,19 +7,28 @@ import os
 class InsertFormulaCommand(sublime_plugin.TextCommand):
 	def run(self, edit): 
 		settings = sublime.load_settings("InsertFormula.sublime-settings")
-		# print(settings["math_preview_exe_path"])
 
-		env = os.environ.copy()
+		command = [os.path.expanduser(settings["math_preview_exe_path"])]
+
+		# Get current selection
+		sel    = self.view.sel()
+		region = next(iter(sel))
+		text   = self.view.substr(region)
+		command.extend(["-i", text])
+
 		if "math_font" in settings:
-			env["MATH_FONT"] = settings["math_font"]
-
-		exe = os.path.expanduser(settings["math_preview_exe_path"])
-
-		sublime.set_timeout_async(lambda: self.get_formula(exe, env), 0)
+			command.extend(["-m", settings["math_font"]])
 
 
-	def get_formula(self, exe, env):
-		process = subprocess.run(exe, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True, env = env)
+		sublime.set_timeout_async(lambda: self.get_formula(command), 0)
+
+
+	def get_formula(self, command):
+		process = subprocess.run(
+			command, 
+			stdout = subprocess.PIPE, 
+			stderr = subprocess.PIPE, 
+		)
 
 		if process.returncode == 0:
 			stdout = process.stdout.decode("utf8")
@@ -27,8 +36,9 @@ class InsertFormulaCommand(sublime_plugin.TextCommand):
 			self.view.run_command("insert_formula_aux", {"formula" : formula})
 
 		else:
+			stderr = process.stderr.decode("utf8")
 			print("!!! ERROR !!!")
-			print(process.stderr.decode("utf8"))
+			print(stderr)
 
 
 
